f393874e6f1ec2e2e0ed137434ebb79b91c5cef2
Raveen Savinda Rathnayake <raveen.s.r@gmail.com>
2019-03-10 21:23:21 +0530

    Update project

--- interec/train_phase.py
+++ interec/train_phase.py
@@ -3,6 +3,8 @@ import logging
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import GridSearchCV
 from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder, StandardScaler
@@ -161,32 +163,57 @@ def scale_scores(csv_file_name):
     return df
 
 
-def find_weight_factors(csv_file_name):
-    df = pd.read_csv(csv_file_name)
+def find_weight_factors(train_data_csv_file_name, test_data_csv_file_name):
+    train_df = pd.read_csv(train_data_csv_file_name)
+    test_df = pd.read_csv(test_data_csv_file_name)
+
     label_encoder = LabelEncoder()
-    df['encoded_integrator'] = label_encoder.fit_transform(df['integrator'])
-    train_labels = df['encoded_integrator']
-    train_feature_set = df[['std_activeness', 'std_file_similarity', 'std_text_similarity']]
+
+    train_labels = label_encoder.fit_transform(train_df['integrator'])
+    train_feature_set = train_df[['std_activeness', 'std_file_similarity', 'std_text_similarity']]
+
+    test_labels = label_encoder.fit_transform(test_df['integrator'])
+    test_feature_set = test_df[['std_activeness', 'std_file_similarity', 'std_text_similarity']]
 
     numeric_features = ['std_activeness', 'std_file_similarity', 'std_text_similarity']
     numeric_transformer = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='median'))])
-    # ('scaler', StandardScaler())])
+        ('imputer', SimpleImputer(strategy='median'))])  # ('scaler', StandardScaler())])
 
     preprocessor = ColumnTransformer(transformers=[
         ('num', numeric_transformer, numeric_features)
     ])
 
-    # X = preprocessor.fit_transform(X_train, y_train)
-    # X.shape
-
     clf = Pipeline(steps=[('preprocessor', preprocessor),
-                          ('classifier', MLPClassifier(hidden_layer_sizes=(11,)))])
+                          ('classifier', MLPClassifier(tol=0.000000001))])
+    parameter_space = {
+        'classifier__hidden_layer_sizes': [(3, 11, 33, 50, 100), (3, 11, 50, 100, 33)],
+        'classifier__activation': ['tanh', 'relu', 'identity', 'logistic'],
+        'classifier__max_iter': [50000],
+        'classifier__solver': ['sgd', 'adam', 'lbfgs'],
+        'classifier__alpha': [0.0001, 0.05, 0.001],
+        'classifier__learning_rate': ['constant', 'adaptive']
+    }
+    clf = GridSearchCV(clf, parameter_space, n_jobs=-1)
     clf.fit(train_feature_set, train_labels)
+
+    # Best paramete set
+    print('Best parameters found:\n', clf.best_params_)
+
+    # All results
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+
+    y_pred = clf.predict(test_feature_set)
+    print(classification_report(test_labels, y_pred))
+    print(accuracy_score(test_labels, y_pred))
 
 
 # initialise_app('scala')
 # calculate_scores(0, 640)
 
 # scale_scores('akka_test_pr_stats.csv')
-find_weight_factors('standardized_akka_test_pr_stats.csv')
+# scale_scores('akka_train_pr_stats.csv')
+
+find_weight_factors('standardized_akka_train_pr_stats.csv', 'standardized_akka_test_pr_stats.csv')
